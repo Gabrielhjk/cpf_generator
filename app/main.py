d223f56ff356ed048755bac97c0c459de1c198ba
Gabriel França <143807216+Gabrielhjk@users.noreply.github.com>
2025-05-16 23:40:11 -0300

    add comments

--- app/main.py
+++ app/main.py
@@ -8,19 +8,22 @@ def main():
     while True:
         create_or_validate_cpf = input('Do you want to create or validate the cpf?\n'
                                        '(Enter "1" to "create" and "2" to "Validate"): ')
- 
+
+        # Estrutura match-case para tratar a escolha do usuário
         match create_or_validate_cpf:
+            # se a opção for 1, cria um novo cpf
             case '1':
                 create_cpf = Create().formatted_cpf()
                 print(create_cpf)
                 break
+            # Se a opção for 2, valida o CPF inserido pelo usuário
             case '2':
                 validate = Validation().validate_user_cpf()
                 print(validate) 
                 break
             case _:
+                # Caso a entrada seja inválida, exibe aviso
                 print('Invalid Option, Try Again!!')
-
 
 if __name__ == '__main__':
     main()
